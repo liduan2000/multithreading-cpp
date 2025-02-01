9d1a90c33ecf2a1d53b3d8766ba51477316d780c
liduan2000 <liduan2000@gmail.com>
2025-02-01 11:55:10 +0800

    add my_atomic demo

---
+++ my_atomic.cpp
@@ -0,0 +1,22 @@
+#include <atomic>
+#include <iostream>
+
+class MyAtomic {
+   private:
+    int i_;
+    double d_;
+    std::string* sptr_;
+};
+
+int main() {
+    std::cout << std::is_trivially_copyable<MyAtomic>::value << std::endl;
+    std::cout << std::is_copy_constructible<MyAtomic>::value << std::endl;
+    std::cout << std::is_move_constructible<MyAtomic>::value << std::endl;
+    std::cout << std::is_copy_assignable<MyAtomic>::value << std::endl;
+    std::cout << std::is_move_assignable<MyAtomic>::value << std::endl;
+    std::cout << std::is_same<MyAtomic, typename std::remove_cv<MyAtomic>::type>::value << std::endl;
+
+    std::atomic<MyAtomic> myAtomic;
+
+    return 0;
+}
