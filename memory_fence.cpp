824026ad778fe9f34e3f51a7d3e10e747437e3db
liduan2000 <liduan2000@gmail.com>
2025-02-01 11:56:10 +0800

    add memory fences(barriers) demo

---
+++ memory_fence.cpp
@@ -0,0 +1,36 @@
+#include <atomic>
+#include <iostream>
+#include <string>
+#include <thread>
+
+std::atomic<std::string*> ptr;
+int data;
+std::atomic<int> atomicData;
+
+void produce() {
+    std::string* p = new std::string("C++ 11");
+    data = 100;
+    atomicData.store(1, std::memory_order_relaxed);
+    std::atomic_thread_fence(std::memory_order_release);
+    ptr.store(p, std::memory_order_relaxed);
+}
+
+void consume() {
+    std::string* p = nullptr;
+    while (!(p = ptr.load(std::memory_order_relaxed))) std::this_thread::yield();
+    std::atomic_thread_fence(std::memory_order_acquire);
+    std::cout << "atomicData: " << atomicData.load(std::memory_order_relaxed) << std::endl;
+    std::cout << "data: " << data << std::endl;
+    std::cout << "p: " << *p << std::endl;
+}
+
+int main() {
+    std::thread producer(produce);
+    std::thread consumer(consume);
+    producer.join();
+    consumer.join();
+
+    delete ptr;
+
+    return 0;
+}
