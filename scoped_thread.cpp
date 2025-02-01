9a173d5f044fb82654553460b2abe880eefe82a4
liduan2000 <liduan2000@gmail.com>
2025-02-01 16:51:41 +0800

    add scoped thread implementation

---
+++ scoped_thread.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdexcept>
+#include <thread>
+#include <utility>
+
+class ScopedThread {
+   public:
+    explicit ScopedThread(std::thread t) : t_(std::move(t)) {
+        if (!t_.joinable()) throw std::logic_error("No thread");
+    }
+    ~ScopedThread() { t_.join(); }
+    ScopedThread(ScopedThread&) = delete;
+    ScopedThread& operator=(ScopedThread const&) = delete;
+
+   private:
+    std::thread t_;
+};
+
+int main() {
+    ScopedThread t(std::thread([] { std::cout << std::this_thread::get_id() << std::endl; }));
+    return 0;
+}
