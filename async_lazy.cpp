6b091d9e39483fffa36aabdcee6a1eedfc799f12
liduan2000 <liduan2000@gmail.com>
2025-02-04 11:44:04 +0800

    add async launch strategies examples

---
+++ async_lazy.cpp
@@ -0,0 +1,25 @@
+#include <chrono>
+#include <future>
+#include <iostream>
+#include <thread>
+
+int main() {
+    auto begin = std::chrono::system_clock::now();
+
+    auto asyncLazy = std::async(std::launch::deferred, [] { return std::chrono::system_clock::now(); });
+
+    auto asyncEager = std::async(std::launch::async, [] { return std::chrono::system_clock::now(); });
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    auto lazyStart = asyncLazy.get() - begin;
+    auto eagerStart = asyncEager.get() - begin;
+
+    auto lazyDuration = std::chrono::duration<double>(lazyStart).count();
+    auto eagerDuration = std::chrono::duration<double>(eagerStart).count();
+
+    std::cout << "asyncLazy evaluated after: " << lazyDuration << " seconds." << std::endl;
+    std::cout << "asyncEager evaluated after: " << eagerDuration << " seconds." << std::endl;
+
+    return 0;
+}
